3d05118f3f97e5bab152869610186b74709bc2ae
Will Rosner <wkrosner@gmail.com>
2020-09-08 18:10:25 -0600

    commit them

---
+++ Question3.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+	//	I would use the freebayes library  to process the BAM files into Variant Calls 
+//https://github.com/ekg/freebayes
+
+	}
+}
